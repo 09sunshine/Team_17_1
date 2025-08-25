cc2c871dda7a0ef234f482208c8f7f2e02939de9
09sunshine <sanshey_230456@aitpune.edu.in>
2025-08-25 17:12:16 +0530

    last

--- frontend/components/event-card.tsx
+++ frontend/components/event-card.tsx
@@ -1,4 +1,4 @@
-
+'use client'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
